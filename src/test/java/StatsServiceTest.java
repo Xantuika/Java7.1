b2fafa4cd3429fdecdcee464451627b782901b5e
Xantuika <105634214+Xantuika@users.noreply.github.com>
2022-06-28 13:47:20 +0300

    Update StatsServiceTest.java

--- src/test/java/StatsServiceTest.java
+++ src/test/java/StatsServiceTest.java
@@ -7,8 +7,7 @@ public class StatsServiceTest {
     @Test
     void countSum() {
         StatsService service = new StatsService();
-        //long[] sales = {14,99,13,4};
-        //long expected = 130;
+        
         long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         long expected = 180;
         long actual = service.countSum(sales);
